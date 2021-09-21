a25711c766c03ba3ab58a77eef4f0ff6a59cb405
SalovS <sviatoslavsalov@yandex.ru>
2021-09-21 14:38:25 +0300

    error correction

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,80 @@
 public class Main {
     public static void main(String[] args){
-        TestArray test = new TestArray();
-
         String[][] arr1 = {{"1","2","3","4"},{"1","2","3","4"},{"1","2","3","4"},{"1","2","3","4"}};
-        String[][] arr2 = {{"1","2","3","4"},{"1","2","3","4"},{"1","2","3","4"},{"1","2","3"}};
+        String[][] arr2 = {{"1","2","3","4"},{"1","2","3","4"},{"1","7483642","3","4"},{"-483642","2","3","4"}};
         String[][] arr3 = {{"1","2","3","4"},{"1","2","3","4"},{"1","2","3","4"}};
         String[][] arr4 = {{"1","2","3","4"},{"1","2","3","4"},{"1","58","3","4"},{"1",null,"3","4"}};
         String[][] arr5 = {{"1","2","3","4"},{"1","2","3","4"},{"1","2147483642","3","4"},{"1","2","3","4"}};
-        test.Start(arr1);
+        String[][] arr6 = {{"-1","-2","-3","-4"},{"-1","-2","-3","-4"},{"-1","-2147483642","-3","-4"},{"1","2","3","4"}};
+        String[][] arr7 = {{"1","2","3","4"},{"1","2","3","4"},{"1","2","3","4"},{"1","2","3"}};
+        calculateNumbers(arr1);
+        calculateNumbers(arr2);
+        calculateNumbers(arr3);
+        calculateNumbers(arr4);
+        calculateNumbers(arr5);
+        calculateNumbers(arr6);
+        calculateNumbers(arr7);
+    }
+
+    private static boolean isCorrectSize(String[][] array){
+        if(array.length != 4){
+            try {
+                throw new MyArraySizeException("Неподходящая мерность массива");
+            } catch (MyArraySizeException e) {
+                return false;
+            }
+        }
+        for(int i = 0; i < 4; i++){
+            if(array[i].length != 4){
+                try {
+                    throw new MyArraySizeException("Размер строки " + i + " не равна 4");
+                } catch (MyArraySizeException e) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void calculateNumbers(String[][]array){
+        if(isCorrectSize(array)) {
+            int number = 0, sum = 0;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    try {
+                        number = Integer.valueOf(array[i][j]);
+                    } catch (NumberFormatException e) {
+                        try {
+                            throw new MayArrayDatAException("Указано неверное значение в массиве в ячейке " +
+                                    "[" + i + "][" + j + "]");
+                        } catch (MayArrayDatAException mayArrayDatAException) {
+                            return;
+                        }
+                    }
+                    if (isCheckLimited(sum, number)) {
+                        sum += number;
+                    } else {
+                        return;
+                    }
+                }
+            }
+            System.out.println("Сумма чисел матрицы : " + sum);
+        }
+    }
+
+    private static boolean isCheckLimited(int sum, int number){
+        if((sum >= 0 && number <= 0) || (sum <= 0 && number >= 0)) {
+            return true;
+        }else if(sum > 0 && number > 0 && Integer.MAX_VALUE - number > sum){
+            return true;
+        }else if(sum > 0 && number > 0 && Integer.MAX_VALUE - number > sum) {
+            return true;
+        }else{
+            try {
+                throw new MayArrayDatAException("Сумма чисел превышает размерность int");
+            } catch (MayArrayDatAException e) {
+                return false;
+            }
+        }
     }
 }
--- src/MayArrayDatAException.java
+++ src/MayArrayDatAException.java
@@ -1,20 +1,6 @@
 public class MayArrayDatAException extends Exception{
-    public MayArrayDatAException() {
-    }
-
     public MayArrayDatAException(String message) {
         super(message);
-    }
-
-    public MayArrayDatAException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public MayArrayDatAException(Throwable cause) {
-        super(cause);
-    }
-
-    public MayArrayDatAException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+        System.err.println(message);
     }
 }
